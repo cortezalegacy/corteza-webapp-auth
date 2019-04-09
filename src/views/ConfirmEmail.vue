7a96c02826f429925d5580cebaf063b47e4208c7
Denis Arh <denis.arh@kendu.si>
2019-04-09 21:34:09 +0200

    Add email confirmation

--- src/views/ConfirmEmail.vue
+++ src/views/ConfirmEmail.vue
@@ -1,10 +1,25 @@
 <template>
   <auth-dialog title="Email confirmation">
-    ....
+    <div v-if="error">
+      {{ error }}
+      <br />
+      <br />
+      <br />
+      Check your email again or try to login again to receive a new token.
+      <br />
+      <br />
+      <router-link :to="{ name: 'login' }">Login</router-link>
+    </div>
+    <div v-else-if="processing">
+      Sending confirmation token ...
+    </div>
   </auth-dialog>
 </template>
 
 <script>
+
+const tokenRegex = /^[a-zA-Z0-9]{32}\d+$/
+
 export default {
   name: 'ConfirmEmail',
   props: {
@@ -13,8 +28,7 @@ export default {
 
   data () {
     return {
-      processing: false,
-
+      processing: true,
       error: null,
     }
   },
@@ -26,15 +40,23 @@ export default {
   },
 
   created () {
+    const token = this.$route.query.token
 
+    if (!tokenRegex.test(token)) {
+      this.error = 'Invalid token'
+    } else {
+      this.confirmToken(token)
+    }
   },
 
   methods: {
-    internalSignup () {
+    confirmToken (token) {
       this.error = null
       this.processing = true
 
-      this.$system.authInternalSignup(this.form).then(r => {
+      this.$system.authInternalConfirmEmail({ token }).then(r => {
+        // @todo store JWT, redirect user back to wherever he came from.
+        this.$logger.log(r)
         this.processing = false
       }).catch(({ message } = {}) => {
         this.error = message
