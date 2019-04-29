9fa933a20d3024f980e895892b53fa3f2ee626e8
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-04-29 08:46:29 +0200

    Add a user check in profile view

--- src/views/ViewProfile.vue
+++ src/views/ViewProfile.vue
@@ -1,17 +1,19 @@
 <template>
   <auth-dialog title="Your Crust profile">
-    <dt>Email</dt>
-    <dd>{{ $auth.user.email }}</dd>
-    <dt>Name</dt>
-    <dd>{{ $auth.user.name }}</dd>
-    <dt>Handle</dt>
-    <dd>{{ $auth.user.handle }}</dd>
-    <div class="footnote">
-      <router-link :to="{ name: 'logout'}">Logout</router-link>
-      <span v-if="internalEnabled">
-        | <router-link :to="{ name: 'change-password'}">Change your password</router-link>
-      </span>
-    </div>
+    <template v-if="$auth.user">
+      <dt>Email</dt>
+      <dd>{{ $auth.user.email }}</dd>
+      <dt>Name</dt>
+      <dd>{{ $auth.user.name }}</dd>
+      <dt>Handle</dt>
+      <dd>{{ $auth.user.handle }}</dd>
+      <div class="footnote">
+        <router-link :to="{ name: 'logout'}">Logout</router-link>
+        <span v-if="internalEnabled">
+          | <router-link :to="{ name: 'change-password'}">Change your password</router-link>
+        </span>
+      </div>
+    </template>
   </auth-dialog>
 </template>
 
