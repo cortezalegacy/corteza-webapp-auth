a3c6ffc48989d7322caff24703e622beae675075
Mia Arh <mia@kendu.si>
2019-07-08 08:19:41 +0200

    Add Card Footer as a separated component

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div slot="footer" class="text-right">
+    <small class="text-muted"><a href="https://cortezaproject.org/" target="_blank">cortezaproject.org</a></small>
+    <span>  |  </span>
+    <small class="text-muted"><a href="https://github.com/cortezaproject/" target="_blank">Github</a></small>
+  </div>
+</template>
