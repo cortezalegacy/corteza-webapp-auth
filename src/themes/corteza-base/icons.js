e2e8931cb6a7825688b33cd2a014b770e03903a1
Denis Arh <denis.arh@kendu.si>
2019-07-12 19:58:31 +0200

    Split theme init & icon loader

---
+++ src/themes/corteza-base/icons.js
@@ -0,0 +1,14 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAt, faUser, faKey } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle, faFacebook, faGithub, faLinkedin, faOpenid } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faAt,
+  faUser,
+  faKey,
+  faGoogle,
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faOpenid,
+)
--- src/themes/corteza-base/index.js
+++ src/themes/corteza-base/index.js
@@ -1,15 +1,2 @@
+import './icons'
 import './index.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAt, faUser, faKey } from '@fortawesome/free-solid-svg-icons'
-import { faGoogle, faFacebook, faGithub, faLinkedin, faOpenid } from '@fortawesome/free-brands-svg-icons'
-
-library.add(
-  faAt,
-  faUser,
-  faKey,
-  faGoogle,
-  faFacebook,
-  faGithub,
-  faLinkedin,
-  faOpenid,
-)
